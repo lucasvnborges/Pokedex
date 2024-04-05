a454758535b8023fec2dd7a0cd25f7c916969d8e
Lucas Borges <lucasvnborges@gmail.com>
2024-04-05 20:35:21 -0300

    feat: add loader

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,11 +1,19 @@
-import React from "react";
-import { WebView } from 'react-native-webview';
+import React, { useState } from "react";
+import { WebView } from "react-native-webview";
+import { Loader } from "../components";
 
 export default function Profile() {
+  const [isLoading, setLoading] = useState(true);
+
   return (
-    <WebView
-      style={{ flex: 1 }}
-      source={{ uri: "https://github.com/lucasvnborges" }}
-    />
+    <>
+      <WebView
+        style={{ flex: 1 }}
+        onLoad={() => setLoading(false)}
+        source={{ uri: "https://github.com/lucasvnborges" }}
+      />
+
+      {isLoading && <Loader />}
+    </>
   );
 }
