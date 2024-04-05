ed0280a80fed18e5ff0752e5c9a7a3e974d0f4ef
Lucas Borges <lucasvnborges@gmail.com>
2024-04-05 20:34:10 -0300

    fix: loader component locking the screen

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -15,7 +15,7 @@ export default function Loader() {
 const styles = StyleSheet.create({
   container: {
     position: "absolute",
-    top: 0,
+    top: 40,
     bottom: 0,
     left: 0,
     right: 0,
